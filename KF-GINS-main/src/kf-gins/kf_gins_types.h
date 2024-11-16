9cbe30c44c429ca0f31f9b7002e1dd6bc7c64a1c
Xiaojilinguier <564004951@qq.com>
2024-11-16 16:56:59 +0800

    第一次导航代码上传

---
+++ KF-GINS-main/src/kf-gins/kf_gins_types.h
@@ -0,0 +1,143 @@
+/*
+ * KF-GINS: An EKF-Based GNSS/INS Integrated Navigation System
+ *
+ * Copyright (C) 2022 i2Nav Group, Wuhan University
+ *
+ *     Author : Liqiang Wang
+ *    Contact : wlq@whu.edu.cn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef KF_GINS_TYPES_H
+#define KF_GINS_TYPES_H
+
+#include <Eigen/Dense>
+#include <iomanip>
+#include <iostream>
+
+#include "common/angle.h"
+// IMU相对于n系的姿态
+typedef struct Attitude {
+    Eigen::Quaterniond qbn;
+    Eigen::Matrix3d cbn;
+    Eigen::Vector3d euler;
+} Attitude;
+
+// 位置速度姿态
+typedef struct PVA {
+    Eigen::Vector3d pos; // 地球坐标系下的大地位置 经纬高
+    Eigen::Vector3d vel; // IMU相对于地球的速度投影在导航参考坐标系下 (北向速度-东向速度-垂向速度)
+    Attitude att; // IMU相对于导航参考坐标系的姿态角
+                  // (四元数、方向余弦矩阵或欧拉角，其中欧拉角定义为航向角-俯仰角-横滚角，ZYX旋转顺序)
+} PVA;
+
+typedef struct ImuError {
+    Eigen::Vector3d gyrbias;
+    Eigen::Vector3d accbias;
+    Eigen::Vector3d gyrscale;
+    Eigen::Vector3d accscale;
+} ImuError;
+
+// 导航状态 位置速度姿态
+typedef struct NavState {
+    Eigen::Vector3d pos; // 经纬高(弧度制)
+    Eigen::Vector3d vel;
+    Eigen::Vector3d euler;
+
+    ImuError imuerror;
+} NavState;
+
+typedef struct ImuNoise {
+    Eigen::Vector3d gyr_arw;      // 陀螺仪角度随机游走
+    Eigen::Vector3d acc_vrw;      // 加计随机游走
+    Eigen::Vector3d gyrbias_std;  // 陀螺仪偏置标准差
+    Eigen::Vector3d accbias_std;  // 加计偏置标准差
+    Eigen::Vector3d gyrscale_std; // 陀螺仪比例因子误差标准差
+    Eigen::Vector3d accscale_std; // 加速度计比例因子误差标准差
+    double corr_time;             // 相关时间常数
+} ImuNoise;
+
+typedef struct GINSOptions {
+
+    // 初始状态和状态标准差
+    // initial state and state standard deviation
+    NavState initstate;
+    NavState initstate_std;
+
+    // IMU噪声参数
+    // imu noise parameters
+    ImuNoise imunoise;
+
+    // 安装参数
+    // install parameters
+    Eigen::Vector3d antlever = {0, 0, 0};
+
+    void print_options() {
+        std::cout << "---------------KF-GINS Options:---------------" << std::endl;
+
+        // 打印初始状态
+        // print initial state
+        std::cout << " - Initial State: " << std::endl;
+        std::cout << '\t' << "- initial position: ";
+        std::cout << std::setprecision(12) << initstate.pos[0] * R2D << "  ";
+        std::cout << std::setprecision(12) << initstate.pos[1] * R2D << "  ";
+        std::cout << std::setprecision(6) << initstate.pos[2] << " [deg, deg, m] " << std::endl;
+        std::cout << '\t' << "- initial velocity: " << initstate.vel.transpose() << " [m/s] " << std::endl;
+        std::cout << '\t' << "- initial attitude: " << initstate.euler.transpose() * R2D << " [deg] " << std::endl;
+        std::cout << '\t' << "- initial gyrbias : " << initstate.imuerror.gyrbias.transpose() * R2D * 3600
+                  << " [deg/h] " << std::endl;
+        std::cout << '\t' << "- initial accbias : " << initstate.imuerror.accbias.transpose() * 1e5 << " [mGal] "
+                  << std::endl;
+        std::cout << '\t' << "- initial gyrscale: " << initstate.imuerror.gyrscale.transpose() * 1e6 << " [ppm] "
+                  << std::endl;
+        std::cout << '\t' << "- initial accscale: " << initstate.imuerror.accscale.transpose() * 1e6 << " [ppm] "
+                  << std::endl;
+
+        // 打印初始状态标准差
+        // print initial state STD
+        std::cout << " - Initial State STD: " << std::endl;
+        std::cout << '\t' << "- initial position std: " << initstate_std.pos.transpose() << " [m] " << std::endl;
+        std::cout << '\t' << "- initial velocity std: " << initstate_std.vel.transpose() << " [m/s] " << std::endl;
+        std::cout << '\t' << "- initial attitude std: " << initstate_std.euler.transpose() * R2D << " [deg] "
+                  << std::endl;
+        std::cout << '\t' << "- initial gyrbias std: " << initstate_std.imuerror.gyrbias.transpose() * R2D * 3600
+                  << " [deg/h] " << std::endl;
+        std::cout << '\t' << "- initial accbias std: " << initstate_std.imuerror.accbias.transpose() * 1e5 << " [mGal] "
+                  << std::endl;
+        std::cout << '\t' << "- initial gyrscale std: " << initstate_std.imuerror.gyrscale.transpose() * 1e6
+                  << " [ppm] " << std::endl;
+        std::cout << '\t' << "- initial accscale std: " << initstate_std.imuerror.accscale.transpose() * 1e6
+                  << " [ppm] " << std::endl;
+
+        // 打印IMU噪声参数
+        // print IMU noise parameters
+        std::cout << " - IMU noise: " << std::endl;
+        std::cout << '\t' << "- arw: " << imunoise.gyr_arw.transpose() * R2D * 60 << " [deg/sqrt(h)] " << std::endl;
+        std::cout << '\t' << "- vrw: " << imunoise.acc_vrw.transpose() * 60 << " [m/s/sqrt(h)] " << std::endl;
+        std::cout << '\t' << "- gyrbias  std: " << imunoise.gyrbias_std.transpose() * R2D * 3600 << " [deg/h] "
+                  << std::endl;
+        std::cout << '\t' << "- accbias  std: " << imunoise.accbias_std.transpose() * 1e5 << " [mGal] " << std::endl;
+        std::cout << '\t' << "- gyrscale std: " << imunoise.gyrscale_std.transpose() * 1e6 << " [ppm] " << std::endl;
+        std::cout << '\t' << "- accscale std: " << imunoise.accscale_std.transpose() * 1e6 << " [ppm] " << std::endl;
+        std::cout << '\t' << "- correlation time: " << imunoise.corr_time / 3600.0 << " [h] " << std::endl;
+
+        // 打印GNSS天线杆臂
+        // print GNSS antenna leverarm
+        std::cout << " - Antenna leverarm: " << antlever.transpose() << " [m] " << std::endl << std::endl;
+    }
+
+} GINSOptions;
+
+#endif // KF_GINS_TYPES_H
